07b9e493d009f8a69867c87c5109f563276334d6
Thiago Panini <panini.development@gmail.com>
2025-07-16 23:41:31 -0300

    feat(VSA): finished Terraform definition for DynamoDB table and IAM role

---
+++ software/vertical_slice_architecture/infra/aws/get-b3-stock-tickers/dynamodb.tf
@@ -0,0 +1,25 @@
+/* -----------------------------------------------------------------------------
+FILE: dynamodb.tf @ get-b3-active-tickers module
+
+DESCRIPTION:
+  This Terraform configuration uses a remote module to provision an AWS DynamoDB
+  table for storing B3 active tickers. The table's name, hash key, range key,
+  and attribute definitions are parameterized via input variables for flexible
+  deployments.
+
+RESOURCES:
+  - module.aws_dynamodb_table:
+    Deploys a DynamoDB table using the tfbox module with configurable properties
+    such as name, hash key, range key, and attributes.
+----------------------------------------------------------------------------- */
+
+
+module "aws_dynamodb_table" {
+  source = "git::https://github.com/ThiagoPanini/tfbox.git?ref=aws/dynamodb-table/v0.1.2"
+
+  name      = var.dynamodb_b3_active_tickers_table_name
+  hash_key  = var.dynamodb_b3_active_tickers_table_hash_key
+  range_key = var.dynamodb_b3_active_tickers_table_range_key
+
+  attributes = var.dynamodb_b3_active_tickers_table_attributes
+}
---
+++ software/vertical_slice_architecture/infra/aws/get-b3-stock-tickers/versions.tf
@@ -0,0 +1,20 @@
+/* -----------------------------------------------------------------------------
+  FILE: versions.tf
+  MODULE: aws/get-b3-stock-tickers
+
+  DESCRIPTION:
+    Configuration of Terraform version and provider used for deployment of
+    module infrastructure resources.
+----------------------------------------------------------------------------- */
+
+
+terraform {
+  required_version = ">= 1.9"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.50"
+    }
+  }
+}
