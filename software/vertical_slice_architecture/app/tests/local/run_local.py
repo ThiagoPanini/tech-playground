e55cbdd2fa0a660e9ab7206473301f5083977438
Thiago Panini <panini.development@gmail.com>
2025-07-16 23:44:13 -0300

    feat(VSA): updating script to test it locally

--- software/vertical_slice_architecture/app/tests/local/run_local.py
+++ software/vertical_slice_architecture/app/tests/local/run_local.py
@@ -1,5 +1,9 @@
+from dotenv import find_dotenv, load_dotenv
 from app.src.features.get_b3_stock_tickers.presentation import get_b3_stock_tickers_presentation
 
+
+# Loading environment variables from .env file
+_ = load_dotenv(find_dotenv())
 
 # Building handlers
 get_b3_stock_tickers_handler = get_b3_stock_tickers_presentation.handler
@@ -10,9 +14,8 @@ FEATURE: Get B3 Stock Tickers
 
 DESCRIPTION:
     This feature provides functionality to scrape and retrieve stock tickers from the B3 (Brazilian
-    Stock Exchange) using a web scraping adapter. It includes the necessary entities, interfaces,
-    and use cases to facilitate the retrieval of stock data.
+    Stock Exchange) using a web scraping adapter.
 """
 
 response = get_b3_stock_tickers_handler(event=None, context=None)
-print(response[0])
+print(response)
