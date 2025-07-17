233bf58d67f27c359bb928832128e49e676342d4
Thiago Panini <panini.development@gmail.com>
2025-07-16 23:44:56 -0300

    feat(VSA): updating use case and presentation layers with output dto and http response mapper class

--- software/vertical_slice_architecture/app/src/features/get_b3_stock_tickers/presentation/get_b3_stock_tickers_presentation.py
+++ software/vertical_slice_architecture/app/src/features/get_b3_stock_tickers/presentation/get_b3_stock_tickers_presentation.py
@@ -1,23 +1,28 @@
 from typing import Any
 
 from app.src.features.cross.infra.adapters.requests_http_client_adapter import RequestsHTTPClientAdapter
-from app.src.features.get_b3_stock_tickers.usecases.get_b3_stock_tickers_usecase import (
+from app.src.features.get_b3_stock_tickers.use_case.get_b3_stock_tickers_use_case import (
     GetB3StockTickersUseCase
 )
 from app.src.features.get_b3_stock_tickers.infra.adapters.fundamentus_b3_stock_tickers_adapter import (
     FundamentusB3StockTickersAdapter
 )
+from app.src.features.get_b3_stock_tickers.infra.repositories.dynamodb_b3_stock_tickers_repository import (
+    DynamoDBB3StockTickersRepository
+)
+from app.src.features.get_b3_stock_tickers.infra.mappers.http_response_mapper import HTTPResponseMapper
 
 
 # Initializing mappers, adapters and repositories
 http_client_adapter = RequestsHTTPClientAdapter()
 b3_stock_tickers_parser_adapter = FundamentusB3StockTickersAdapter()
-
+b3_stock_tickers_repository = DynamoDBB3StockTickersRepository()
 
 # Initializing use case
 use_case = GetB3StockTickersUseCase(
     http_client_adapter=http_client_adapter,
-    b3_stock_tickers_parser_adapter=b3_stock_tickers_parser_adapter
+    b3_stock_tickers_parser_adapter=b3_stock_tickers_parser_adapter,
+    b3_stock_tickers_repository=b3_stock_tickers_repository
 )
 
 
@@ -34,4 +39,6 @@ def handler(event: dict[str, Any], context: Any) -> Any:
         Any: The result of the use case execution, typically a list of stock tickers.
     """
 
-    return use_case.execute()
+    output_dto = use_case.execute()
+
+    return HTTPResponseMapper.map(output_dto)
